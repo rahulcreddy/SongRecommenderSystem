88aabe3218a1f30c658167be5d941cc8b2b0ca0f
Rahul Chada <rrchada98@gmail.com>
2023-04-19 13:57:26 -0400

    Updated folder path

---
+++ artist_info/fetch_track_info.py
@@ -0,0 +1,32 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import os
+
+#cid = os.environ.get('CLIENT')
+#secret = os.environ.get('CLIENT_SECRET')
+
+cid = os.environ['CLIENT']
+secret = os.environ['CLIENT_SECRET']
+
+# open the text file
+#with open(data_file, 'r') as f:
+#    cid = f.readline().strip()
+#    secret = f.readline().strip()
+
+#Authentication - without user
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+def get_album_cover_url(track_id):
+    album_cover_url = sp.tracks([track_id])['tracks'][0]['album']['images'][1]['url']
+    return album_cover_url
+
+def get_track_url(track_id):
+    track_url = sp.tracks([track_id])['tracks'][0]['external_urls']['spotify']
+    return track_url
+
+def get_track_info(track_dataframe):
+    track_dataframe['album_cover_url'] = track_dataframe['track_id'].apply(get_album_cover_url)
+    track_dataframe['track_url'] = track_dataframe['track_id'].apply(get_track_url)
+    return track_dataframe
